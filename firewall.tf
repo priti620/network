4c7a07cbde7dfaf74b9ec6104d3a2b5c7558a752
priti620 <121668165+priti620@users.noreply.github.com>
2023-08-09 15:05:00 +0530

    Create firewall.tf

---
+++ firewall.tf
@@ -0,0 +1,26 @@
+resource "google_compute_firewall" "fw-1" {
+  name    = "allow-only-gke-cluster"
+  network = google_compute_network.vpc.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["3306"]
+  }
+
+  priority = 1000
+
+  source_ranges = ["10.10.10.0/24"]
+}
+
+resource "google_compute_firewall" "fw-2" {
+  name    = "allow-only-authorized-networks"
+  network = google_compute_network.vpc.name
+
+  allow {
+    protocol = "tcp"
+  }
+
+  priority = 1000
+
+  source_ranges = var.authorized_source_ranges
+}
